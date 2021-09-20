abc94eaeec57744d50a17d2f99fa1bcd755ced3d
srihariksr27 <87702822+srihariksr27@users.noreply.github.com>
2021-09-20 08:24:06 +0530

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-import re
-from flask import Flask,render_template
-import speech_recognition as sr
-app=Flask(__name__)
-
-@app.route("/",methods=["GET","POST"])
-def index():
-    return render_template("index.html")
